7829a6eda9c4a43aa007ef026f9dab1964dc77b6
Éverton Toffanetto <evertondgn@hotmail.com>
2021-09-19 21:42:35 -0300

    style: change flex-direction card

--- src/components/CardWrapper/styles.ts
+++ src/components/CardWrapper/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.section`
+  position: relative;
   border-radius: 0.8rem;
   padding: 2rem;
   background-image: linear-gradient(182deg, #eceff2, #fff);
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-between;
   box-shadow: -0.4rem -0.4rem 0.6rem #fff, 0.3rem 0.3rem 0.6rem #1a2d5a29;
 `
